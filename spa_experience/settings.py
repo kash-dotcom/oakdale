b51a8b176fff92fe61ba9c0e5895f0cd458a6b62
kash-dotcom <katyashworth27@gmail.com>
2025-03-08 17:11:35 +0000

    Admin Site 1858416950

--- spa_experience/settings.py
+++ spa_experience/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
+# print("------- DATABASE URL -------")
+# print(os.environ.get("DATABASE_URL"))
+# print("----------------------------")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +82,15 @@ WSGI_APPLICATION = 'spa_experience.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
