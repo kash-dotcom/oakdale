19a2ab44af1db2aeb9257adff6c0580cc0bdf43c
kash-dotcom <katyashworth27@gmail.com>
2025-03-11 08:41:47 +0000

    Register models to admin site - 1858416950

--- guest/admin.py
+++ guest/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    Guest, Reservation, Experience, Payment, Review, Enhancements, ValuedGuest
+)
 
 # Register your models here.
+admin.site.register(Guest)
+admin.site.register(Reservation)
+admin.site.register(Payment)
+admin.site.register(Experience)
+admin.site.register(Enhancements)
+admin.site.register(Review)
+admin.site.register(ValuedGuest)
