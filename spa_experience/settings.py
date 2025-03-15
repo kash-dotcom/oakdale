bab00103b4d2002fb165e80fa500ada1318e1e96
kash-dotcom <katyashworth27@gmail.com>
2025-03-15 13:21:31 +0000

    Generate and add a new secret key for application security

--- spa_experience/settings.py
+++ spa_experience/settings.py
@@ -29,9 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-ja+w(%467i5yn_uv!b9333y-r-x=*et!ahg@o#gb3!!!-)7#37'
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
