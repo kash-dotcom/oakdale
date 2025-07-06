92e35c077044ad07da6181d9c8fe5e24cb9e39c2
kash-dotcom <katyashworth27@gmail.com>
2025-07-06 12:29:24 +0100

    Fix validation issues caused by jQuery global variable usage

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,27 +1,18 @@
-
+/* global $, document */
 
 // Materialize
 $(document).ready(function () {
   $('.sidenav').sidenav();
+  $('.datepicker').datepicker();
+  $('select').formSelect();
   $('.carousel.carousel-slider').carousel({
     fullWidth: true,
-    indicators: true
+    indicators: true,
   });
   // $('.modal').modal();
   $('login_success').tooltip();
   $('.parallax').parallax({ responsiveThreshold: 0 });
   console.log('Materialize loaded');
-});
-
-//Modal//
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems, {});
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.datepicker');
-  var instances = M.Datepicker.init(elems, format = 'dd-mm-yyyy', autoclose = true, showClearBtn = true);
 });
 
 // https://www.youtube.com/watch?v=4NqAiqdjMI8&list=PLCC34OHNcOtpRfBYk-8y0GMO4i1p1zn50&index=13&ab_channel=Codemy.com
@@ -47,24 +38,10 @@ $(document).on('click', '#add-reservation', function (e) {
 
 });
 
-// Materialize select
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
-});
-
-
 // delete reservation
   // Handle delete reservation button click
   $(document).on('click', '.delete_reservation', function() {
     var reservationId = $(this).data('id');
     $('#delete-reservation-id').val(reservationId);
     console.log('Delete reservation button clicked with reservation ID:', reservationId);
-  });
-
-  // Log form submission
-  $('#delete-reservation-form').on('submit', function(e) {
-    e.preventDefault(); 
-    var reservationId = $('#delete-reservation-id').val();
-      this.submit();    
   });
